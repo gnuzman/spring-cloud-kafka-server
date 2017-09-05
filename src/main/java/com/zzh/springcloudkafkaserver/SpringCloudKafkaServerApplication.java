67b7208f3d7b7f39e550e005f4f29b6e8cbc19af
gnuzman <94644378@qq.com>
2017-09-05 09:27:46 +0800

    create

---
+++ src/main/java/com/zzh/springcloudkafkaserver/SpringCloudKafkaServerApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.springcloudkafkaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudKafkaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudKafkaServerApplication.class, args);
+	}
+}
